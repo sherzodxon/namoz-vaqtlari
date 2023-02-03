da5020031b494b7f4ba226412a5c2750a5ca7386
sherzodxon <yarmatxonovsherzodxon@gmail.com>
2023-02-03 18:01:25 +0500

    map togirlandi3

--- src/pages/map/map.js
+++ src/pages/map/map.js
@@ -7,7 +7,7 @@ const Map=()=>{
             <div className="map-container container">
             <iframe src={`https://maps.google.com/maps?q=${location.latitude}, ${location.longitude}&output=embed`} width="450" height="470" ></iframe>
             </div>
-            
+            <div className="map-botto"></div>
         </div>
     )
 }
