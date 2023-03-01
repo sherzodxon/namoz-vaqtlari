e4984221385a928933f3cae2227435772eb4be0d
sherzodxon <yarmatxonovsherzodxon@gmail.com>
2023-03-01 16:56:37 +0500

    names is finished

--- src/pages/live/live.js
+++ src/pages/live/live.js
@@ -1,13 +1,23 @@
 import { Link } from "react-router-dom"
 import "../live/live.scss"
+import LinkBox from "../../assets/components/link-box/link-box"
+import Control from "../../assets/components/control/control"
+import Header from "../../assets/components/header/header"
 const Live =()=>{
 return(
   <div className="live">
-    <div className="live-header">
-      <h3 className="live-title">Live</h3>
-    </div>
+    <Header/>
+    <div className="background"></div>
     <div className="live-container container">
-        <ul className="live-video-list">
+      <LinkBox/>
+      <div className="live-main zikr-main main">
+      <Control className={"live-control"}/>
+      <div className="live-header">
+      <h3 className="live-title">Live</h3>
+      </div>
+    <div className="live-body zikr-body">
+    
+    <ul className="live-video-list">
             <li className="live-video">
             <iframe width="450" height="253" className="live-iframe" src="https://www.youtube.com/embed/zgkPMys1aXc">
              </iframe>
@@ -32,6 +42,9 @@ return(
              </iframe>
             </li>
         </ul>
+    </div>
+      </div>
+        
         <div className="live-bottom suralar-bottom">
             <Link to={"/"} className="bottom-button back-button suralar-bottom-button" />
             </div>
--- src/pages/map/map.js
+++ src/pages/map/map.js
@@ -1,17 +1,29 @@
 import { Link } from 'react-router-dom';
 import {useLocation} from '../../contexts/context'
 import '../map/map.scss'
+import LinkBox from "../../assets/components/link-box/link-box"
+import Control from "../../assets/components/control/control"
+import Header from "../../assets/components/header/header"
 const Map=()=>{
-    const {location,setLocation} = useLocation();
+    const {location} = useLocation();
   
     return(
         <div className="map">
+            <Header />
+            <div className="background"></div>
             <div className="map-container container">
-                <div className="map-header">
+                <LinkBox/>
+                <div className="map-main zikr-main main">
+                  
+                    <div className="map-body zikr-body">
+                    <Control className={"map-control"}/>
+                    <div className="map-header">
                     <h3 className="map-country">{location.country}</h3>
                     <p className="map-city">{location.locality}</p>
                 </div>
-            <iframe className='map-iframe' src={`https://maps.google.com/maps?q=${location.latitude}, ${location.longitude}&output=embed`} ></iframe>
+                 <iframe className='map-iframe' src={`https://maps.google.com/maps?q=${location.latitude}, ${location.longitude}&output=embed`} ></iframe>
+                    </div>
+                </div>
             <div className="map-bottom">
             <Link to={"/"} className="map-back-button bottom-button back-button suralar-button"/>
             </div>
